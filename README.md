9437b0b1a3480106f06b8cf4cd43c1bbbe1fc695
Nikolay Komolov <nkspb13@gmail.com>
2018-01-04 17:28:46 +0300

    Added TodoApp

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Here you can find some of my web projects.
 <h2>ScoreKeeper</h2> <b>Score Keeper</b> Count the score of 2 players up to the max number specified.
 
 <h2>ColorGame</h2> <b>Color Game</b> Guess a color given its RGB value.
+
+<h2>TodoApp</h2> <b>ToDo App</b> A simple ToDo list app.
---
+++ TodoApp/assets/js/app.js
@@ -0,0 +1,22 @@
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(e){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e){
+	if (e.which === 13) {
+		// grab new todo text
+		let todoText = $(this).val();
+		// clear input
+		$(this).val("");
+		// append new todo
+		let todoHTML = "<li><span>X</span> " + todoText + "</li>";
+		$("ul").append(todoHTML);
+	}
+});
